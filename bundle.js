0c6d86073d4d9679a1ca263eb1a724a19576ecfa
Sasha Bogicevic <sasha.bogicevic@iohk.io>
2023-12-12 22:28:15 +0100

    Display websocket status

--- bundle.js
+++ bundle.js
@@ -1,10 +1,24 @@
 const client = new WebSocket("ws://127.0.0.1:4001");
+const status = document.querySelector("#status");
 
 client.addEventListener("message", e => {
     const msg = JSON.parse(e.data);
     console.log(msg);
 });
 
+client.onopen = (e) => {
+    writeStatus("CONNECTED");
+};
 
+client.onclose = (e) => {
+    writeStatus("DISCONNECTED");
+};
 
+client.onerror = (e) => {
+    writeStatus(`ERROR: ${e.data}`);
+};
+
+function writeStatus(message) {
+    status.innerHTML = `<p>STATUS: ${message}</p>`;
+}
 
